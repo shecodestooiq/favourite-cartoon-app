11875cc29915399e6ffd5822ce7cf3751b38eb69
ulaali <ulaali513@yahoo.com>
2023-08-11 16:19:37 +0300

    add favorite feasure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CartoonsList from '../Components/CartoonList';
+
+export default function Home() {
+  return (
+    <div>
+      <CartoonsList />
+    </div>
+  );
+}
